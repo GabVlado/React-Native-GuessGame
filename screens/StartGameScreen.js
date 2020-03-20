b528086b739375324ac9a29dc39e00d2a57862e9
Gabriel V <gv.valdivieso2309@gmail.com>
2020-03-20 15:04:41 -0500

    54 StartGameScreen 2 - Button Reset and Confirm

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput ,Button } from 'react-native'
 
 const StartGameScreen = props  => {
     return (
         <View style={styles.screen}>
-            <Text>The Game Screen</Text>
+            <Text style={styles.title}>Start a New Game</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => {}} />  
+                    <Button title="Confirm"  onPress={() => {}}/>  
+                </View>
+            </View>
         </View>
     )
 }
@@ -14,9 +22,23 @@ const styles = StyleSheet.create({
     screen:{
         flex: 1 ,
         padding: 10,
-        alignItems: 'center', // Ejes Horizontales 
-
+        alignItems: 'center', // Ejes Horizontales        
+    },
+    title:{
+        fontSize: 20,
+        marginVertical: 10, // Replace marginBottom  and marginTop
         
+    },
+    inputContainer:{
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center' // Ejes horizontales 
+    },
+    buttonContainer:{
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 })
 
